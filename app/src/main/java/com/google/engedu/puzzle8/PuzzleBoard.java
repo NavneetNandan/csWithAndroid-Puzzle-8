03d4be49bed9a99fa78a6e64acc74cc7211e3075
NavneetNandan <navneet.nandan8@gmail.com>
2016-10-09 15:36:23 +0530

    generalized for n number of tiles

--- app/src/main/java/com/google/engedu/puzzle8/PuzzleBoard.java
+++ app/src/main/java/com/google/engedu/puzzle8/PuzzleBoard.java
@@ -11,7 +11,7 @@ import java.util.PriorityQueue;
 
 public class PuzzleBoard {
 
-    private static final int NUM_TILES = 3;
+    private static final int NUM_TILES = 4;
     private static final int[][] NEIGHBOUR_COORDS = {
             { -1, 0 },
             { 1, 0 },
@@ -29,10 +29,10 @@ public class PuzzleBoard {
         for (int i = 0; i <NUM_TILES ; i++) {
             for (int j = 0; j <NUM_TILES ; j++) {
                 Bitmap tile=Bitmap.createBitmap(bitmap,j*widthOfTile,i*widthOfTile,widthOfTile,widthOfTile);
-                tiles.add(new PuzzleTile(tile,3*i+j));
+                tiles.add(new PuzzleTile(tile,NUM_TILES*i+j));
             }
         }
-        tiles.remove(8);
+        tiles.remove(NUM_TILES*NUM_TILES-1);
         tiles.add(null);
         steps=0;
         previousBoard=null;
@@ -118,7 +118,7 @@ public class PuzzleBoard {
             if (tile==null){
                 ArrayList<PuzzleBoard> neighbours=new ArrayList<>();
                 for (int[] NEIGHBOUR_COORD : NEIGHBOUR_COORDS) {
-                    if (k / 3 + NEIGHBOUR_COORD[1] < 3 && k / 3 + NEIGHBOUR_COORD[1] >= 0 && k % 3 + NEIGHBOUR_COORD[0] < 3 && k % 3 + NEIGHBOUR_COORD[0] >= 0) {
+                    if (k / NUM_TILES + NEIGHBOUR_COORD[1] < NUM_TILES && k / NUM_TILES + NEIGHBOUR_COORD[1] >= 0 && k % NUM_TILES + NEIGHBOUR_COORD[0] < NUM_TILES && k % NUM_TILES + NEIGHBOUR_COORD[0] >= 0) {
                         PuzzleBoard temp = new PuzzleBoard(this);
                         temp.swapTiles(k, k + NEIGHBOUR_COORD[0] + NUM_TILES * NEIGHBOUR_COORD[1]);
                         neighbours.add(temp);
@@ -134,12 +134,13 @@ public class PuzzleBoard {
         int manhattanDistance=0;
         for (int i = 0; i <tiles.size() ; i++) {
             if(tiles.get(i)!=null) {
-                int columnChange = Math.abs(i % 3 - tiles.get(i).getNumber() % 3);
-                int rowChange = Math.abs(i / 3 - tiles.get(i).getNumber() / 3);
+                int columnChange = Math.abs(i % NUM_TILES - tiles.get(i).getNumber() % NUM_TILES);
+                int rowChange = Math.abs(i / NUM_TILES - tiles.get(i).getNumber() / NUM_TILES);
                 manhattanDistance+= columnChange+rowChange;
             }else {
-                int columnChange = Math.abs(i % 3 - 8 % 3);
-                int rowChange = Math.abs(i / 3 - 8 / 3);
+                int columnChange = Math.abs(i % NUM_TILES - (NUM_TILES*NUM_TILES-1) % NUM_TILES);
+                int rowChange = Math.abs(i / NUM_TILES - (NUM_TILES*NUM_TILES-1) / NUM_TILES);
+                manhattanDistance+=columnChange+rowChange;
             }
         }
         return manhattanDistance+steps;
